cf1a2c2ab89e441e8a5ad0a873784106eb17de0a
Ram Sah <mohan2036@gmail.com>
2020-06-21 14:01:31 -0400

    added new .js file to and start writing code

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+//to make a function available after the document is loaded
+$(document).ready(function(){
+    //set const variable 
+    const test = false;
+
+    //get times moment 
+    const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+    //declearing for test in non-standard hrs
+    let nowHour24 = moment().format('H');
+    let nowHour12 = moment().format('h');
+
+    //setting time for testing after hours
+    if (test) {
+        nowHour24 = 13;
+        nowHour12 = 1;
+    }
+    let $dateHeading = $('#currentDay');
+    $dateHeading.text(now);
+    console.log(dateHeading);
+});
